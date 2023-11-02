6484c8572b9c73958a2ee8ce52c099454f6822c9
Ishan Dasgupta <ishan.dasguptar@gmail.com>
2023-11-02 10:25:11 -0500

    Add search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,151 @@
+<script setup>
+defineProps({
+  parentFilterData: Object,
+});
+</script>
+
+<template>
+  <div class="colFlex">
+    <label for="product-filter"
+      >Search for products (product name contains string)</label
+    >
+    <input id="product-filter" placeholder="" />
+
+    <div class="rowFlex">
+      <div class="colFlex">
+        <label for="min-cores-filter">Minimum Cores?</label>
+        <input
+          id="min-cores-filter"
+          placeholder="0"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+
+      <div class="colFlex">
+        <label for="max-cores-filter">Maximum Cores?</label>
+        <input
+          id="max-cores-filter"
+          placeholder="Infinity"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+    </div>
+
+    <div class="rowFlex">
+      <div class="colFlex">
+        <label for="min-litho-filter">Minimum Lithography?</label>
+        <input
+          id="min-litho-filter"
+          placeholder="0"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+
+      <div class="colFlex">
+        <label for="max-litho-filter">Maximum Lithography?</label>
+        <input
+          id="max-litho-filter"
+          placeholder="Infinity"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+    </div>
+
+    <div class="rowFlex">
+      <div class="colFlex">
+        <label for="min-threads-filter">Minimum Threads?</label>
+        <input
+          id="min-threads-filter"
+          placeholder="0"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+
+      <div class="colFlex">
+        <label for="max-threads-filter">Maximum Threads?</label>
+        <input
+          id="max-threads-filter"
+          placeholder="Infinity"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+    </div>
+
+    <div class="rowFlex">
+      <div class="colFlex">
+        <label for="min-base-filter">Minimum Base Freq.?</label>
+        <input
+          id="min-base-filter"
+          placeholder="0"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+
+      <div class="colFlex">
+        <label for="max-base-filter">Maximum Base Freq.?</label>
+        <input
+          id="max-base-filter"
+          placeholder="Infinity"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+    </div>
+
+    <div class="rowFlex">
+      <div class="colFlex">
+        <label for="min-turbo-filter">Minimum Turbo Freq.?</label>
+        <input
+          id="min-turbo-filter"
+          placeholder="0"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+
+      <div class="colFlex">
+        <label for="max-turbo-filter">Maximum Turbo Freq.?</label>
+        <input
+          id="max-turbo-filter"
+          placeholder="Infinity"
+          type="number"
+          oninput="validity.valid||(value='');"
+        />
+      </div>
+    </div>
+
+    <button @click="applyFilter">Filter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["parentFilterData"],
+  data: function () {
+    return {};
+  },
+  computed: {},
+  methods: {
+    applyFilter() {
+      this.parentFilterData.searchString = document.querySelector("#product-filter").value;
+      this.parentFilterData.minCores = document.querySelector("#min-cores-filter").value == "" ? 0 : parseInt(document.querySelector("#min-cores-filter").value);
+      this.parentFilterData.maxCores = document.querySelector("#max-cores-filter").value == "" ? 1000 : parseInt(document.querySelector("#max-cores-filter").value);
+      this.parentFilterData.minLitho = document.querySelector("#min-litho-filter").value == "" ? 0 : parseInt(document.querySelector("#min-litho-filter").value);
+      this.parentFilterData.maxLitho = document.querySelector("#max-litho-filter").value == "" ? 1000 : parseInt(document.querySelector("#max-litho-filter").value);
+      this.parentFilterData.minThreads = document.querySelector("#min-threads-filter").value == "" ? 0 : parseInt(document.querySelector("#min-threads-filter").value);
+      this.parentFilterData.maxThreads = document.querySelector("#max-threads-filter").value == "" ? 1000 : parseInt(document.querySelector("#max-threads-filter").value);
+      this.parentFilterData.minBase = document.querySelector("#min-base-filter").value == "" ? 0 : parseInt(document.querySelector("#min-base-filter").value);
+      this.parentFilterData.maxBase = document.querySelector("#max-base-filter").value == "" ? 1000 : parseInt(document.querySelector("#max-base-filter").value);
+      this.parentFilterData.minTurbo = document.querySelector("#min-turbo-filter").value == "" ? 0 : parseInt(document.querySelector("#min-turbo-filter").value);
+      this.parentFilterData.maxTurbo = document.querySelector("#max-turbo-filter").value == "" ? 1000 : parseInt(document.querySelector("#max-turbo-filter").value);
+    },
+  },
+};
+</script>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,6 +1,7 @@
 <script setup>
 import TableData from "./TableData.vue";
 import HideBar from "./HideBar.vue";
+import SearchBar from "./SearchBar.vue";
 </script>
 
 <template>
@@ -13,20 +14,22 @@ import HideBar from "./HideBar.vue";
         @clearedAll="hideShowALLstatus"
       />
 
-      <p>Search Bar</p>
+      <SearchBar :parent-filter-data="this.filterData" />
 
-      <div class="rowFlex">
-        <button @click="prevPage">previous</button>
+      <div class="colFlex">
         <p>
           Showing products {{ startDataIndex }}-{{ endDataIndex }} out of
           {{ totalProducts }}
         </p>
-        <button @click="nextPage">next</button>
+        <div class="rowFlex">
+          <button @click="prevPage">previous</button>
+          <button @click="nextPage">next</button>
+        </div>
       </div>
     </div>
     <!-- Main Table Design -->
     <TableData :product-data="productDataBystatus" />
-    <!-- End of Table Design -->
+    <!-- End of Table Design -->s
   </div>
 </template>
 
@@ -40,6 +43,19 @@ export default {
       allCheck: false,
       totalProducts: data.length,
       pageIndex: 0,
+      filterData: {
+        searchString: "",
+        minCores: 0,
+        maxCores: 1000,
+        minLitho: 0,
+        maxLitho: 1000,
+        minThreads: 0,
+        maxThreads: 1000,
+        minBase: 0,
+        maxBase: 1000,
+        minTurbo: 0,
+        maxTurbo: 1000,
+      },
     };
   },
   mounted() {
@@ -60,11 +76,33 @@ export default {
       let statusSet = new Set();
 
       data.forEach((element, index) => {
+        let productName = element.Product;
         let status = element.Status;
         let cores = element.Cores;
+        let litho = element.Lithography;
+        let threads = element.Threads;
+        let base = element.Base_Freq;
+        let turbo = element.Max_Turbo_Freq;
 
         // push status to set
         statusSet.add(status);
+
+        if (!productName.includes(this.filterData.searchString)) return;
+        if (this.filterData.minCores > cores) return;
+        if (this.filterData.maxCores < cores) return;
+
+        if (this.filterData.minLitho > litho) return;
+        if (this.filterData.maxLitho < litho) return;
+
+        if (this.filterData.minThreads > threads) return;
+        if (this.filterData.maxThreads < threads) return;
+
+        if (this.filterData.minBase > base) return;
+        if (this.filterData.maxBase < base) return;
+
+        if (this.filterData.minTurbo > turbo) return;
+        if (this.filterData.maxTurbo < turbo) return;
+
         if (this.hidestatus.includes(status)) return; // Hide by status
         this.totalProducts++;
 
@@ -84,24 +122,24 @@ export default {
 
       statusSet.forEach((status) => {
         for (const cores in tmp[status]) {
-          tmp[status][cores].forEach((product, index)=>{
+          tmp[status][cores].forEach((product, index) => {
             rowNumber++;
             // console.log(rowNumber + " " + this.startDataIndex)
-            if (rowNumber < this.startDataIndex) return;;
+            if (rowNumber < this.startDataIndex) return;
             if (rowNumber > this.endDataIndex) return;
 
-            if(!answer[status]) answer[status] = [];
-            if(!answer[status][cores]) answer[status][cores] = [];
+            if (!answer[status]) answer[status] = [];
+            if (!answer[status][cores]) answer[status][cores] = [];
 
             answer[status][cores].push(product);
-          })
+          });
         }
       });
 
       if (oldTotalProducts != this.totalProducts) {
         this.pageIndex = 0;
       }
-      
+
       return {
         status: [...statusSet],
         data: answer,
